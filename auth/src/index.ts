596f673c8c0e6c40288b554ea1358fc2f8962de1
ghislaingenay <ghislain.gny@gmail.com>
2022-12-05 10:59:13 +0700

    Add routing ingress-nginx server config file + testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,13 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-app.listen(3000, () => {
-  console.log('Listening on  port 3000');
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
 });
+
+app.listen(3000, () => {
+  console.log('v1');
+  console.log('Listening on port 3000');
+});
+
+//Ingress
