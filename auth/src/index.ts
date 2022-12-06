5f33433f5343b44b1aceba7f2e458d8fd9fb46b3
ghislaingenay <ghislain.gny@gmail.com>
2022-12-06 15:24:19 +0700

    Create basic handling errors middleware + error subclasses

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-
+import { errorHandler } from './middlewares/error-handler';
 import { json } from 'body-parser';
 
 const app = express();
@@ -17,6 +17,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.use(errorHandler);
 // Listening port
 app.listen(3000, () => {
   console.log('v1');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -19,11 +21,13 @@ router.post(
   (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
+      // This error will  be picked up by the error handler middleware
     }
 
     const { email, password } = req.body;
     console.log('Creating a user ...');
+    throw new DatabaseConnectionError();
     res.send({});
   }
 );
