bb9aa4e41f21609d33dbcee28c0bcfe458c738df
ghislaingenay <ghislain.gny@gmail.com>
2022-12-06 10:55:19 +0700

    Implement basics routes for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,17 +1,24 @@
 import express from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 import { json } from 'body-parser';
 
 const app = express();
+
+// Middleware
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-});
+// Routes
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
+// Listening port
 app.listen(3000, () => {
   console.log('v1');
   console.log('Listening on port 3000');
 });
-
-//Ingress
