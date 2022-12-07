3d95e72340617a4b296d7b1a6b14c6073a152177
ghislaingenay <ghislain.gny@gmail.com>
2022-12-07 11:18:35 +0700

    Change return data from MongoDB

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -16,17 +16,30 @@ interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
+  {
+    toJSON: {
+      transform(doc: any, ret: any) {
+        // Make direct changes to the JSON OBJECT
+        delete ret.password;
+        delete ret.__v;
+        ret.id = ret._id;
+        delete ret._id;
+      }
+    }
   }
-});
+);
 
 // Not possible to add ES6 syntax, otherwise it will be overwritten (context of the file)
 userSchema.pre('save', async function (done) {
