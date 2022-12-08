291f177f99925060a6a813fe34c2ffa8dea48cd8
ghislaingenay <ghislain.gny@gmail.com>
2022-12-08 10:29:57 +0700

    Add server side auth + recover cookie

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -6,8 +6,13 @@ import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
-});
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  // requireAuth,
+  (req, res) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };
