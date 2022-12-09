265ce48ed614723ffa195994fad8adaa62457560
ghislaingenay <ghislain.gny@gmail.com>
2022-12-09 08:50:35 +0700

    Add mongo DB url as env variables for auth/tickets service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,13 +4,19 @@ const start = async () => {
   if (!process.env.JWT_TOKEN) {
     throw new Error('JWT_TOKEN must be defined');
   }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {}
 };
 
 start();
+
 // Listening port
 app.listen(3000, () => {
   console.log('v1');
