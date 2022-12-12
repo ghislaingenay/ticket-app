ab6caed2d891a8499221d2f613eed68bda9f93cf
ghislaingenay <ghislain.gny@gmail.com>
2022-12-12 21:30:34 +0700

    Implement Listener class for nats to gather boilerplate code

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 // @ts-ignore
 import { randomBytes } from 'crypto';
 
@@ -48,3 +48,50 @@ process.on('SIGINT', () => {
 process.on('SIGTERM', () => {
   client.close();
 });
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return (
+      this.client
+        .subscriptionOptions()
+        // setDeliverAllAvailable is needed even if setDurableName: add a new service online for the first time only => all events will be sent to new service
+        .setDeliverAllAvailable()
+        .setManualAckMode(true)
+        .setAckWait(this.ackWait)
+        .setDurableName(this.queueGroupName)
+    );
+  }
+
+  listen() {
+    const subcription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subcription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  // How to parse a buffer data.toString('utf8')
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
+}
