a330432ce3e9eb279ba804d5048e1d15b7a81d40
ghislaingenay <ghislain.gny@gmail.com>
2022-12-13 21:36:34 +0700

    Create TicketCreatedPublisher for ticket:created event

---
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketCreatedEvent } from '@gg-tickets/common';
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,7 +12,9 @@ const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
-  } catch (err) {}
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 start();
