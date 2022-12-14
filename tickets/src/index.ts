84d74f7d1ae39ee7af1246ec9f643fde43239748
ghislaingenay <ghislain.gny@gmail.com>
2022-12-14 22:00:24 +0700

    Add exit process for nats in index.ts

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,17 @@ const start = async () => {
 
   try {
     await natsWrapper.connect('tickets', 'hdjd', 'http://nats-srv:4222');
+    // Add force shutdown
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    // When you try to close the client or disconnect from running server, verify if user restart server or close it
+    process.on('SIGINT', () => natsWrapper.client.close());
+    // SIG are signal sent to this process => intercept request on the program and close the program first and close the connection
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
