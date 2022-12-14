d8ef6a4738c0f4ba3cd1784fe570304f06b007be
ghislaingenay <ghislain.gny@gmail.com>
2022-12-14 22:29:21 +0700

    ReAdd files to nats-test (previously sent to npm packages) + test NATS listener

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,10 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from '../../../common/src/events/base-listener';
-import { Subjects } from './subjects';
+import { Subjects } from '../../../common/src/events/subjects';
 import { TicketCreatedEvent } from './ticket-created-event';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     // Any type of modification is done here. Add something to database for exple
