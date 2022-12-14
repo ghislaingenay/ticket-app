71b38325125e2f011f9562c283805c0a4fe80561
ghislaingenay <ghislain.gny@gmail.com>
2022-12-14 20:41:00 +0700

    Connect nats to index.ts using natsWrapper class

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,7 @@
 import mongoose, { Mongoose } from 'mongoose';
+import { natsWrapper } from './nats-wrapper';
 import { app } from './_app';
+
 const start = async () => {
   if (!process.env.JWT_TOKEN) {
     throw new Error('JWT_TOKEN must be defined');
@@ -10,6 +12,7 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('tickets', 'hdjd', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
