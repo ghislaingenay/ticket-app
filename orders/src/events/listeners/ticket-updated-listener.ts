b57255ffdfff372166c31db9e1fb72c8057dc844
ghislaingenay <ghislain.gny@gmail.com>
2022-12-23 17:02:25 +0700

    Implement fn in models to findOne with the correct version

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,7 +8,7 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   queueGroupName = ORDERS_SERVICE;
   async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
     console.log('Heyy');
-    const ticket = await Ticket.findById(data.id);
+    const ticket = await Ticket.findByEvent(data);
     if (!ticket) {
       throw new Error('Ticket not found');
     }
