ba832c31e493498538ba720ab52f7248dd202f3f
ghislaingenay <ghislain.gny@gmail.com>
2022-12-26 09:20:47 +0700

    Use real mongodb db to fix issues about mongo image can't be pulled (monolith approach)

--- orders/src/models/orders.ts
+++ orders/src/models/orders.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { TicketDoc } from './ticket';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@gg-tickets/common';
 
 interface OrderAttrs {
@@ -50,6 +51,9 @@ const orderSchema = new mongoose.Schema(
     }
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
